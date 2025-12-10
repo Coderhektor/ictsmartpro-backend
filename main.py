8024ff7f6b95d69873976e6813239290ee87bee0
Coderhektor <168417177+Coderhektor@users.noreply.github.com>
2025-12-10 07:42:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+
+# BU KISIM EKLENECEK (en alta)
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
