72b97f5f0ea20b5228c07139d4cfacd985402eab
Coderhektor <168417177+Coderhektor@users.noreply.github.com>
2026-01-04 01:12:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -873,3 +873,4 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=False)
 
+sddsfjlkdsfldslşfdskfdsldslfşldşsşd
